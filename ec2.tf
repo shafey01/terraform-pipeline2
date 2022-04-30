9687bb14d550e60d96b4f2f2b1749499f8b0c64e
shafey01 <shafey0101@gmail.com>
2022-04-30 22:57:35 +0200

    l

--- ec2.tf
+++ ec2.tf
@@ -14,33 +14,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
-resource "aws_instance" "web2" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_1.id
-  vpc_security_group_ids = ["sg-0ab35918e37a236cc"]
-  key_name = "terraformkey"
+  subnet_id   = aws_subnet.public_1.id
 
   tags = {
-    Name = "bastion_server"
-  }
-  provisioner "local-exec" {
-      command = "echo ${aws_instance.web2.public_ip} > ./ansible/inventory"
-
-    
+    Name = "HelloWorld"
   }
 }
-
-
-resource "aws_instance" "web1" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.private_1___.id
-
-  tags = {
-    Name = "public_instance"
-  }
- 
-}
-
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "access_key" {
-  type = string
-}
-variable "secret_key" {
-  type = string
-}
+# variable "access_key" {
+#   type = string
+# }
+# variable "secret_key" {
+#   type = string
+# }
